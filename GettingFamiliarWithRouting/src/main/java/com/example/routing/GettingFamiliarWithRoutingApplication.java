a22bafdf4d6fb111af431c9c487f3d68939268fd
smolaaardy <50385566+smolaaardy@users.noreply.github.com>
2019-08-12 15:43:49 -0500

    Add files via upload

---
+++ GettingFamiliarWithRouting/src/main/java/com/example/routing/GettingFamiliarWithRoutingApplication.java
@@ -0,0 +1,12 @@
+package com.example.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class GettingFamiliarWithRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingFamiliarWithRoutingApplication.class, args);
+	}
+
+}
